99c8cdff3e31ae9218cb07892998cdd352d97912
ali-jin <alina.jin.contact@gmail.com>
2025-02-21 15:27:25 +0100

    Modify the Notifications

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <form>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="eamil" name="email" />
+          <input type="email" id="email" name="email" />
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" />
           <button type="submit">OK</button>
--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,31 @@
 import React from "react";
 import './Notifications.css';
+import { getLatestNotification } from "./utils";
 
 function Notifications() {
+  const handleClose = () => {
+    console.log('Close button has been clicked')
+  }
   return (
     <div className="Notifications">
+      <button
+        style={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          background: 'transparent',
+          border: 'none',
+          fontSize: '20px',
+          cursor: 'pointer'
+        }}
+        aria-label="Close"
+        onClick={handleClose}>x</button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
