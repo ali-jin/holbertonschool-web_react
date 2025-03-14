18068ebeddd751d255fe00e3bb63d26ed127992a
ali-jin <alina.jin.contact@gmail.com>
2025-03-14 16:00:05 +0100

    update index.js and notifications.js

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Notifications from './Notifications/Notifications';
+import Notifications from '../Notifications/Notifications';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <div className="root">
-      <App />
-    </div>
-  </React.StrictMode>
-);
-ReactDOM.render(<App />, document.getElementById('root'));
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
