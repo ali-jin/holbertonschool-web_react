43e6a09dc67c5ad4f98e871402e36e3632aa47d2
ali-jin <alina.jin.contact@gmail.com>
2025-03-24 10:57:24 +0100

    update tests

--- react_component/task_0/dashboard/src/App/App.test.js
+++ react_component/task_0/dashboard/src/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from './App'
+import App from './App.js'
 import Notifications from '../Notifications/Notifications.js'
 import Header from '../Header/Header.js'
 import Login from '../Login/Login.js'
