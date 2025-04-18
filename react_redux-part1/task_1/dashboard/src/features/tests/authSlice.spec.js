8dc9fece3d432277a6f26228503c260c8299eae2
ali-jin <alina.jin.contact@gmail.com>
2025-04-18 14:40:44 +0200

    Authentication Slice

---
+++ react_redux-part1/task_1/dashboard/src/features/tests/authSlice.spec.js
@@ -0,0 +1,33 @@
+import authReducer, { login, logout } from "../auth/authSlice";
+
+describe("authSlice", () => {
+  const initialState = {
+    user: { email: "", password: "" },
+    isLoggedIn: false,
+  };
+
+  it("should return the initial state", () => {
+    expect(authReducer(undefined, { type: "unknown" })).toEqual(initialState);
+  });
+
+  it("should handle login", () => {
+    const user = { email: "test@example.com", password: "password" };
+    const action = login(user);
+    const expectedState = {
+      user,
+      isLoggedIn: true,
+    };
+
+    expect(authReducer(initialState, action)).toEqual(expectedState);
+  });
+
+  it("should handle logout", () => {
+    const loggedInState = {
+      user: { email: "test@example.com", password: "password" },
+      isLoggedIn: true,
+    };
+    const action = logout();
+
+    expect(authReducer(loggedInState, action)).toEqual(initialState);
+  });
+});
