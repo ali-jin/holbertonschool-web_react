a66ec2904f7fc029044fcd350cbfa52e297a1230
ali-jin <alina.jin.contact@gmail.com>
2025-05-16 15:40:00 +0200

    update selectors and notifications

---
+++ react_redux_connectors_and_providers/task_8/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite';
+import PropTypes from 'prop-types'
+import BodySection from './BodySection'
+
+class BodySectionWithMarginBottom extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    children: null
+  }
+
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    )
+  }
+}
+
+// Define styles using Aphrodite
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '40px',
+  },
+});
+
+export default BodySectionWithMarginBottom
